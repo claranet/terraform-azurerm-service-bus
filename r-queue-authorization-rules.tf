70b22a6ecb6090666457f729632f59cbbc9c4e94
Jérôme Respaut <jerome.respaut@fr.clara.net>
2022-03-18 17:23:31 +0100

    AZ-515: Option to use Azure CAF naming provider to name resources

--- r-queue-authorization-rules.tf
+++ r-queue-authorization-rules.tf
@@ -1,6 +1,6 @@
 resource "azurerm_servicebus_queue_authorization_rule" "reader" {
   for_each            = toset(local.queues_reader)
-  name                = "${split("|", each.key)[1]}-reader"
+  name                = var.use_caf_naming ? azurecaf_name.servicebus_queue_auth_rule_reader[each.key].result : "${split("|", each.key)[1]}-reader"
   namespace_name      = azurerm_servicebus_namespace.servicebus_namespace[split("|", each.key)[0]].name
   queue_name          = azurerm_servicebus_queue.queue[each.key].name
   resource_group_name = var.resource_group_name
@@ -12,7 +12,7 @@ resource "azurerm_servicebus_queue_authorization_rule" "reader" {
 
 resource "azurerm_servicebus_queue_authorization_rule" "sender" {
   for_each            = toset(local.queues_sender)
-  name                = "${split("|", each.key)[1]}-sender"
+  name                = var.use_caf_naming ? azurecaf_name.servicebus_queue_auth_rule_sender[each.key].result : "${split("|", each.key)[1]}-sender"
   namespace_name      = azurerm_servicebus_namespace.servicebus_namespace[split("|", each.key)[0]].name
   queue_name          = azurerm_servicebus_queue.queue[each.key].name
   resource_group_name = var.resource_group_name
@@ -24,7 +24,7 @@ resource "azurerm_servicebus_queue_authorization_rule" "sender" {
 
 resource "azurerm_servicebus_queue_authorization_rule" "manage" {
   for_each            = toset(local.queues_manage)
-  name                = "${split("|", each.key)[1]}-manage"
+  name                = var.use_caf_naming ? azurecaf_name.servicebus_queue_auth_rule_manage[each.key].result : "${split("|", each.key)[1]}-manage"
   namespace_name      = azurerm_servicebus_namespace.servicebus_namespace[split("|", each.key)[0]].name
   queue_name          = azurerm_servicebus_queue.queue[each.key].name
   resource_group_name = var.resource_group_name
--- r-servicebus-queues.tf
+++ r-servicebus-queues.tf
@@ -1,6 +1,6 @@
 resource "azurerm_servicebus_queue" "queue" {
   for_each            = toset(local.queues_list)
-  name                = split("|", each.key)[1]
+  name                = var.use_caf_naming ? azurecaf_name.servicebus_queue[each.key].result : split("|", each.key)[1]
   resource_group_name = var.resource_group_name
   namespace_name      = azurerm_servicebus_namespace.servicebus_namespace[split("|", each.key)[0]].name
 
